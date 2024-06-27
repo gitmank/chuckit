d61d2fb685bf7ef10e5825be4c86997fc2cd0455
Manomay Kagalkar <manomay111@gmail.com>
2024-06-27 07:54:59 +0530

    added demo link on home page

--- chuckit.xyz/app/page.jsx
+++ chuckit.xyz/app/page.jsx
@@ -3,7 +3,10 @@ import UpvotePanel from "@/components/UpvotePanel";
 
 export default function Home() {
   return (
-    <main id="home-main" className="flex min-h-screen w-screen flex-col items-center justify-between p-8">
+    <main
+      id="home-main"
+      className="flex min-h-screen w-screen flex-col items-center justify-between p-8"
+    >
       <nav className="flex flex-row w-full h-max p-4 justify-between items-center self-start">
         <div className="flex flex-row items-center gap-4">
           <Image
@@ -15,17 +18,25 @@ export default function Home() {
           />
           <h1 className="text-2xl font-bold text-blue-400">chuckit.xyz</h1>
         </div>
-        <h1 className="text-2xl font-bold text-blue-400">v0.0.0</h1>
+        <a href="/demo" className="text-2xl font-bold text-blue-400">
+          try v0.1.0
+        </a>
       </nav>
       <section className="flex flex-col items-center text-center gap-16">
-        <h2 className="text-4xl font-bold text-blue-400">chuck files across devices and continents!</h2>
+        <h2 className="text-4xl font-bold text-blue-400">
+          chuck files across devices and continents!
+        </h2>
         <UpvotePanel />
       </section>
       <section className="flex flex-row items-center text-base text-gray-400">
         <p>idea by&nbsp;</p>
-        <a target="_blank" className="underline" href="https://manomay.co">mank</a>
+        <a target="_blank" className="underline" href="https://manomay.co">
+          mank
+        </a>
         <p>&nbsp;for&nbsp;</p>
-        <a target="_blank" className="underline" href="https://buildspace.so">n&w s5</a>
+        <a target="_blank" className="underline" href="https://buildspace.so">
+          n&w s5
+        </a>
       </section>
     </main>
   );
