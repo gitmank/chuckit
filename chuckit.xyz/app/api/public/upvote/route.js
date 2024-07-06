4cba0e8f06120ed8c03f440c96c2d4b420dee969
Manomay Kagalkar <manomay111@gmail.com>
2024-07-06 11:54:57 +0530

    standardised some api responses

--- chuckit.xyz/app/api/public/upvote/route.js
+++ chuckit.xyz/app/api/public/upvote/route.js
@@ -6,13 +6,13 @@ export async function GET(req, res) {
         const url = new URL(req.url.slice())
         const key = url.searchParams.get("key");
         if (!key) {
-            return NextResponse.json({ error: "No key provided." }, { status: 400 });
+            return NextResponse.json({ error: "invalid data" }, { status: 400 });
         }
         const upvotes = await client.get(key);
         return NextResponse.json({ upvotes }, { status: 200 });
     } catch (error) {
         console.error("GET /upvotes error");
-        return NextResponse.json({ error: "Internal server error." }, { status: 500 });
+        return NextResponse.json({ error: "internal error" }, { status: 500 });
     }
 }
 
@@ -21,14 +21,14 @@ export async function POST(req, res) {
         const url = new URL(req.url.slice())
         const key = url.searchParams.get("key");
         if (!key) {
-            return NextResponse.json({ error: "No key provided." }, { status: 400 });
+            return NextResponse.json({ error: "invalid data" }, { status: 400 });
         }
         await client.incr(key);
         const upvotes = await client.get(key);
         return NextResponse.json({ upvotes }, { status: 200 });
     } catch (error) {
         console.error('POST /upvotes error');
-        return NextResponse.json({ error: "Internal server error." }, { status: 500 });
+        return NextResponse.json({ error: "internal error" }, { status: 500 });
     }
 }
 
@@ -37,13 +37,13 @@ export async function PUT(req, res) {
         const url = new URL(req.url.slice())
         const key = url.searchParams.get("key");
         if (!key) {
-            return NextResponse.json({ error: "No key provided." }, { status: 400 });
+            return NextResponse.json({ error: "invalid data" }, { status: 400 });
         }
         const { upvotes } = await req.json();
         await client.set(key, upvotes);
         return NextResponse.json({ status: 200 });
     } catch (error) {
         console.error('POST /upvotes error');
-        return NextResponse.json({ error: "Internal server error." }, { status: 500 });
+        return NextResponse.json({ error: "internal error" }, { status: 500 });
     }
 }
